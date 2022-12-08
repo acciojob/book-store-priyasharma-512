142f8be2494826fa29e169ecb41c22ce310bde08
priyasharma-512 <sbpriya512@github.com>
2022-12-08 22:17:16 +0530

    test4

--- src/main/java/com/driver/Book.java
+++ src/main/java/com/driver/Book.java
@@ -11,8 +11,8 @@ public class Book {
      private String author;
 
 
-     public Book(int id, String name, String genre, String author) {
-          this.id = id;
+     public Book( String name, String genre, String author) {
+
           this.name = name;
           this.genre = genre;
           this.author = author;
